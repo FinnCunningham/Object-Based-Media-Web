1502eb82c173d070359ee389b80b3bf1ab7b33ef
FinnCunningham <finnc2000@gmail.com>
2022-03-16 16:47:42 +0000

    Web sockets working

--- public/js/tv_sockets.js
+++ public/js/tv_sockets.js
@@ -1,15 +1,21 @@
 $(function () {
-    var socket = io();
+    // var socket = io.connect('https://s5117817.bucomputing.uk', { path: '/node/socket.io/'});
     socket.on('start_video', (msg) => {
+        console.log("BACK" + socket.id)
         console.log("STARTING VIDEO...")
-        $('#tv-content').html("<video id='videoClip'><source src='/assets/videos/FINAL Highlights 2021 US Open Pool Championship.mp4' type='video/mp4'></video>");    
+        $('#tv-content').html("<video id='videoClip' class='video_player' width='90%' height='90%'><source src='/assets/videos/FINAL Highlights 2021 US Open Pool Championship.mp4' type='video/mp4'></video>");    
         document.getElementById("videoClip").load();
     })
     
-    socket.on('play', function(msg){
+    socket.on('play_video', function(msg){
+        console.log("BACK" + socket.id)
         console.log("Start Clip");
+        document.getElementById("videoClip").play();
     });
 
+    socket.on('pause_video', () => {
+        document.getElementById("videoClip").pause();
+    })
 
 
 });
--- server.js
+++ server.js
@@ -3,44 +3,84 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
-const io = new Server(server);
+var cors = require('cors');
 
+const io = new Server(server, { 
+  path: '/socket.io',
+  cors: {
+    // origin: "https://s5117817.bucomputing.uk/node",
+    origin: '*',
+    methods: ["GET", "POST"],
+    // transports: ['websocket', 'polling'],
+    // credentials: true
+  },
+  // allowEIO3: true
+});
 // set the public folder to serve public assets
 app.use(express.static(__dirname + '/public'));
+app.use(cors()); 
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/pages/homepage.html');
 });
 
-let interval;
-
+// let interval;
 io.on('connection', (socket) => {
-    console.log('a user connected');
-    socket.on('play', function(){
-		console.log('play clip');
-		io.emit('play');
-	});
-    socket.on('start_video', function(){
+  
+  console.log('a user connected from ');
+  console.log(io.sockets.adapter.rooms)
+  socket.on('join', (room) => {
+    socket.join(room);
+    console.log("JOINED ROOM " + room)
+    console.log(io.sockets.adapter.rooms)
+  })
+
+  // socket.on('rooms', () => {
+  //   socket.emit('rooms', io.sockets.adapter.rooms);
+  // })  
+
+  socket.on('start_video', function(){
 		console.log('Starting Video');
 		io.emit('start_video');
 	});
 
-    if (interval) {
-        clearInterval(interval);
-      }
-      interval = setInterval(() => getApiAndEmit(socket), 1000);
-      socket.on("disconnect", () => {
-        console.log("Client disconnected");
-        clearInterval(interval);
-    });
+  socket.on('secondary_device_connected', function(){
+		console.log('Secondary device connected');
+    // let rooms = io.sockets.adapter.rooms;
+    let rooms = getActiveRooms(io)
+    console.log(rooms)
+		io.emit('secondary_device_connected', {"rooms": rooms});
+	});
+  socket.on('play_video', function(id){
+		console.log('play clip');
+    console.log(id)
+		io.emit('play_video');
+	});
+  socket.on('pause_video', function(){
+		io.emit('pause_video');
+	});
 });
 
-const getApiAndEmit = socket => {
-    const response = new Date();
-    // Emitting a new message. Will be consumed by the client
-    socket.emit("FromAPI", response);
-};
 
-server.listen(8080, () => {
-  console.log('listening on *:8080');
+io.on("connect_error", (e) => {
+  // revert to classic upgrade
+  console.log(e)
+  io.opts.transports = ["polling", "websocket"];
+});
+
+function getActiveRooms(io) {
+  // Convert map into 2D list:
+  // ==> [['4ziBKG9XFS06NdtVAAAH', Set(1)], ['room1', Set(2)], ...]
+  const arr = Array.from(io.sockets.adapter.rooms);
+  // Filter rooms whose name exist in set:
+  // ==> [['room1', Set(2)], ['room2', Set(2)]]
+  const filtered = arr.filter(room => !room[1].has(room[0]))
+  // Return only the room name: 
+  // ==> ['room1', 'room2']
+  const res = filtered.map(i => i[0]);
+  return res;
+}
+
+server.listen(40074, () => {
+  console.log('listening on *:40074');
 });
